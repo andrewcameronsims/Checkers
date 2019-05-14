e4448d5f9cb17383bc1ddb6fb000001a5bcd95cb
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-14 22:12:10 +1000

    WIP

--- javascripts/checkers.js
+++ javascripts/checkers.js
@@ -1,5 +1,8 @@
+const whitePiece = "<div class='white-piece' onclick='activatePiece(event)'></div>";
+const blackPiece = "<div class='black-piece' onclick='activatePiece(event)'></div>";
+
 function getValidMoves(position) {
-  
+  // This is a stub for finding valid moves
 }
 
 function activatePiece(e) {
@@ -12,15 +15,48 @@ function activatePiece(e) {
   validMoves = getValidMoves(position)
 }
 
-function resetGame() {
+function removePieces() {
+  // Get arrays including all pieces
   let blacks = document.getElementsByClassName('black-piece');
   let whites = document.getElementsByClassName('white-piece');
-  for (let i = 0; i < blacks.length; i++) {
+
+  // Remove them from board
+  for (let i = 1; i < blacks.length; i++) {
     let element = blacks[i]
     element.parentNode.removeChild(element)
   }
-  for (let i = 0; i < whites.length; i++) {
+  for (let i = 1; i < whites.length; i++) {
     let element = whites[i]
     element.parentNode.removeChild(element)
   }
+}
+
+function placeRow(el, row, offset) {
+  let squares = Array.prototype.slice.call( document.getElementsByClassName('square') )
+  squares = squares.slice((row * 8) - 8 + offset, (row * 8) + 1)
+  for (i = 0; i < row.length; i++) {
+    if ((i + 1) % 2 == 0) {
+      squares[i].innerHTML(el)
+    }
+  }
+}
+
+function placeWhites() {
+  placeRow(whitePiece, 6, 1)
+  placeRow(whitePiece, 7, 0)
+  placeRow(whitePiece, 8, 1)
+}
+
+function placeBlacks() {
+  placeRow(blackPiece, 1, 0)
+  placeRow(blackPiece, 2, 1)
+  placeRow(blackPiece, 3, 0)
+}
+
+function resetGame() {
+  removePieces()
+  removePieces()
+  removePieces()
+  placeWhites()
+  placeBlacks()
 }
