8c72be72e83442d4bc1d667d9f1dd0d07f82deb9
Andrew Sims <andrew.cameron.sims@gmail.com>
2019-05-21 18:12:45 +1000

    Refactor grid helpers

--- javascripts/checkers.js
+++ javascripts/checkers.js
@@ -3,29 +3,44 @@ const whiteStarts = [41, 43, 45, 47, 48, 50, 52, 54, 57, 59, 61, 63]
 
 const globalVariables = {
   pieceSelected: false,
+  validLetters: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+  validNumbers: [1, 2, 3, 4, 5, 6, 7, 8],
 }
 
 // Grid Helpers
 
 const nextChar = (char) => {
-  
+  return String.fromCharCode(c.charCodeAt(0) + 1);
 }
 
 const prevChar = (char) => {
-
+  return String.fromCharCode(c.charCodeAt(0) - 1);
 }
 
 const getDiagonals = (piece, id) => {
   const position = id.split('/');
-  // Error handling and sanitisation
-  
 
   // Get diagonals
-  const upper_left = [nextChar(position[0]), position[1] - 1].join('/');
-  const upper_right = [nextChar(position[0]), position[1] + 1].join('/');
-  const lower_left = [prevChar(position[0]), position[1] - 1].join('/');
-  const lower_right = [prevChar(position[0]), position[1] + 1].join('/');
-  return [upper_left, upper_right, lower_left, lower_right]
+  const u_l = [prevChar(position[0]), position[1] - 1];
+  const u_r = [prevChar(position[0]), position[1] + 1];
+  const l_l = [nextChar(position[0]), position[1] - 1];
+  const l_r = [nextChar(position[0]), position[1] + 1];
+
+  // Error handling; we don't want positions off the board
+  const checked = [u_l, u_r, l_l, l_r].map((pos) => {
+    if (!validLetters.includes(pos[0]) || !validNumbers.includes(pos[1])) {
+      return undefined;
+    } else {
+      return pos;
+    }
+  })
+
+  return {
+    u_l: checked[0],
+    u_r: checked[1],
+    l_l: checked[2],
+    l_r: checked[3],
+  }
 }
 
 // Game Logic
@@ -33,16 +48,17 @@ const getDiagonals = (piece, id) => {
 const getValidMoves = (piece, position) => {
   // Get the two forward diagonal positions
   const position_index = position.id;
+  const diagonals = getDiagonals(position_index);
   if (piece.classList.contains('white-piece')) {
-    const positions = getDiagonals(position_index).slice(0,2);
+    const positions = [diagonals.u_l, diagonals.u_r];
   } else if (piece.classList.contains('black-piece')){
-    const positions = getDiagonals(position_index).slice(2,4);
+    const positions = [diagonals.l_l, diagonals.l_r];
   };
 
   // Add visual cues and onclicks to the valid positions
   // Can move forward diagonally if a piece is not in the way
   positions.forEach(square => {
-    return square.classList.toggle('selected-tile');
+    square.classList.toggle('selected-tile');
   });
   // Can jump forward diagonally if an enemy piece present
   
